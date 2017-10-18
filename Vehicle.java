90a1c3d88c7f5dde91297677bf6201fe961d2aac
Julián Marrades <julian.marrades@hotmail.es>
2017-10-18 12:47:40 +0200

    Add files via upload

--- Vehicle.java
+++ Vehicle.java
@@ -4,8 +4,11 @@ public class Vehicle {
 
 	DrawingCanvas canvas;
 
-	final int AGENT_WIDTH = 28;
-	final int AGENT_HEIGHT = 28;
+	final int AGENT_WIDTH = 20;
+	final int AGENT_HEIGHT = 20;
+
+	final double maxSpeed = 1;
+	final double maxForce = 2;
 
 	JVector position;
 	JVector speed;
@@ -14,13 +17,20 @@ public class Vehicle {
 	public Vehicle(DrawingCanvas canvas) {
 		this.canvas = canvas;
 		position = new JVector((int) (Math.random()*canvas.getWidth()), (int) (Math.random()*canvas.getHeight()));
-		speed = new JVector((Math.random()*3) - 3, (Math.random()*3) - 3);
+		speed = new JVector(0, 0);
 		acceleration = new JVector(0, 0);
 	}
 
 	public void update() {
-		speed = JVector.add(acceleration, speed);
-		position = JVector.add(speed, position);
+		speed = JVector.add(speed, acceleration);
+		position = JVector.add(position, speed);
+	}
+
+	public void applyBehaviours() {
+	}
+
+	public void applyForce(JVector force) {
+		acceleration = JVector.add(acceleration, force);
 	}
 
 	public void draw(Graphics g) {
@@ -40,6 +50,35 @@ public class Vehicle {
 		else if (position.intY() < 0) {
 			position.y = canvas.getHeight();
 		}
+	}
+
+	public void seek(JVector goal) {
+		JVector desired =  JVector.sub(goal, position);
+
+		double mapspeed = 0;
+		double desMag = desired.getMagnitude();
+		
+		if (desMag < 75) {
+			mapspeed = mapData(desMag, 0, 75, 0, maxSpeed);
+		}
+		else {
+			mapspeed = maxSpeed;
+		}
+
+		desired.setMagnitude(mapspeed);
+		JVector steering = JVector.sub(desired, speed);
+		steering.setMagnitude(maxForce);
+		applyForce(steering);
+	}
+
+	private double mapData(double value, double min1, double max1, double min2, double max2) {
+		double tempValue = value - min1;
+		double interval1 = max1 - min1;
+		double interval2 = max2 - min2;
+		double perc = tempValue / interval1;
+		double tempMap = perc * interval2;
+		double mapped = tempMap + min2;
+		return mapped;
 	}
 
 }
